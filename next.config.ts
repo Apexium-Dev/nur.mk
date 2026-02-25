d902b90bcc65ad90c741d4952654d26d73c24580
Mahmut Miftaroski <miftarosky@hotmail.com>
2026-02-25 06:03:09 +0100

    feat: update next.config.ts with configuration options for output, images, and basePath

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/nur.mk",
 };
 
 export default nextConfig;
